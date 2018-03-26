4cb4c4b613be5af766e3f3b848f7a2cb8b747d93
lamhh26 <hoanglamuet@gmail.com>
2018-03-26 21:07:09 +0700

    crud comment question

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :text, length: {minimum: Settings.comment.text.minimum_length,
+                            maximum: Settings.comment.text.maximum_length}
 end
